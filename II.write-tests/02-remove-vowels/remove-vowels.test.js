2474d2c9606c73b7c2e2e9996343df55a3c1d9e6
Ignacio <igonzalez@mobdev.cl>
2022-04-18 21:58:37 -0400

    tests added to exercise II.2

--- II.write-tests/02-remove-vowels/remove-vowels.test.js
+++ II.write-tests/02-remove-vowels/remove-vowels.test.js
@@ -4,7 +4,7 @@ describe("Recive a word and return the word without vowels", () => {
   it("for the word 'Samuel' it will return 'S-m--l' ", () => {
     //arrange
     const word = 'Samuel';
-    const expected = 'S-m--l'
+    const expected = 'S-m--l';
 
     //act
     const result = removeVowels(word);
@@ -12,5 +12,29 @@ describe("Recive a word and return the word without vowels", () => {
     //assert
     expect(result).toEqual(expected);
 
-  })
+  });
+
+  it("for the word 'zxcvbnm' it will return 'zxcvbnm'", () => {
+    //arrange
+    const word = 'zxcvbnm';
+    const expected = 'zxcvbnm';
+
+    //act 
+    const result = removeVowels(word);
+
+    //assert
+    expect(result).toEqual(expected);
+  });
+
+  it("for the word 'aeiou' it will return '-----'", () => {
+    //arrange
+    const word = 'aeiou';
+    const expected = '-----';
+
+    //act 
+    const result = removeVowels(word);
+
+    //assert
+    expect(result).toEqual(expected);
+  });
 });
