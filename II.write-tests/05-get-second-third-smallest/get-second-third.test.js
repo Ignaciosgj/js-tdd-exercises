2cb4beefcc5f4454df6f655a3ca17acc9fe8886b
Ignacio <igonzalez@mobdev.cl>
2022-04-19 20:41:08 -0400

    exercises II.4 to II.7 done

--- II.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ II.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -1,3 +1,65 @@
+const { getSecondThird } = require("./get-second-third");
+
+describe("Get the second and third smallest number", () => {
+    it("for array [90, 5, 11, 8, 6] it will return [6, 8]", () => {
+        //arrange
+        const input = [90, 5, 11, 8, 6];
+        const numbers = [90, 5, 11, 8, 6];
+        const expected = [6, 8];
+
+        //act
+        const result = getSecondThird(input);
+
+        //assert
+        expect(result).toEqual(expected);
+        expect(numbers).toEqual(input);
+    });
+
+    it("if function getSecondThird doesn't recive an array it will throw an error", () => {
+        //arrange
+        const invalidInput1 = 11;
+        const invalidInput2 = '';
+        const invalidInput3 = undefined;
+        const invalidInput4 = true;
+        const invalidInput5 = {name: 'nombre'};
+
+        //act & assert
+        expect(() => getSecondThird(invalidInput1)).toThrow("error");
+        expect(() => getSecondThird(invalidInput2)).toThrow("error");
+        expect(() => getSecondThird(invalidInput3)).toThrow("error");
+        expect(() => getSecondThird(invalidInput4)).toThrow("error");
+        expect(() => getSecondThird(invalidInput5)).toThrow("error");
+    });
+
+    it("if function getSecondThird doesn't recive an array of numbers it will throw an error", () => {
+        //arrange
+        const invalidInput1 = 11;
+        const invalidInput2 = '';
+        const invalidInput3 = [''];
+        const invalidInput4 = undefined;
+        const invalidInput5 = [undefined];
+        const invalidInput6 = true;
+        const invalidInput7 = [true];
+        const invalidInput8 = {name: 'nombre'};
+        const invalidInput9 = [{name: 'nombre'}];
+
+        //act & assert 
+        expect(() => getSecondThird(invalidInput1)).toThrow("error");
+        expect(() => getSecondThird(invalidInput2)).toThrow("error");
+        expect(() => getSecondThird(invalidInput3)).toThrow("error");
+        expect(() => getSecondThird(invalidInput4)).toThrow("error");
+        expect(() => getSecondThird(invalidInput5)).toThrow("error");
+        expect(() => getSecondThird(invalidInput6)).toThrow("error");
+        expect(() => getSecondThird(invalidInput7)).toThrow("error");
+        expect(() => getSecondThird(invalidInput8)).toThrow("error");
+        expect(() => getSecondThird(invalidInput9)).toThrow("error");
+    });
+
+    it("if function getSecondThird doesn't recive any params it will throw an error", () => {
+        //act & assert
+        expect(() =>getSecondThird()).toThrow("error");
+    })
+});
 // example
 // input = [90, 5, 11, 8, 6];
 // expected output = [6, 8];
--- II.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ II.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,47 @@
+const { getEven } = require("./get-even-numbers");
+
+describe("Get even numbers", () => {
+    it("for array [22, 13, 73, 82, 4] it will return [22, 82, 4]", () => {
+        //arrange
+        const input = [22, 13, 73, 82, 4];
+        const expected = [22, 82, 4];
+
+        //act
+        const result = getEven(input);
+
+        //assert
+        expect(result).toEqual(expected);
+    });
+
+    it("if function getEven doesn't recive an array of numbers it will throw an error", () => {
+        //arrange
+        const invalidInput1 = 11;
+        const invalidInput2 = '';
+        const invalidInput3 = [''];
+        const invalidInput4 = undefined;
+        const invalidInput5 = [undefined];
+        const invalidInput6 = true;
+        const invalidInput7 = [true];
+        const invalidInput8 = {name: 'nombre'};
+        const invalidInput9 = [{name: 'nombre'}];
+
+        //act & assert
+        expect(() => getEven(invalidInput1)).toThrow("error");
+        expect(() => getEven(invalidInput2)).toThrow("error");
+        expect(() => getEven(invalidInput3)).toThrow("error");
+        expect(() => getEven(invalidInput4)).toThrow("error");
+        expect(() => getEven(invalidInput5)).toThrow("error");
+        expect(() => getEven(invalidInput6)).toThrow("error");
+        expect(() => getEven(invalidInput7)).toThrow("error");
+        expect(() => getEven(invalidInput8)).toThrow("error");
+        expect(() => getEven(invalidInput9)).toThrow("error");
+    });
+
+    it("if function getEven doesn't recive any params it will throw an error", () => {
+        //act & assert
+        expect(() => getEven()).toThrow("error");
+    });
+});
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
