2d41cbbd4be5d8e845b37c546735e6ffd349776d
Ignacio González <ignacio.sgj@gmail.com>
2022-04-25 10:17:02 -0400

    async test 1

--- II.write-tests/09-test-async/async-1.js
+++ II.write-tests/09-test-async/async-1.js
@@ -2,12 +2,22 @@ const fetch = require("node-fetch");
 
 const getRepos = function(repoName) {
   return fetch(repoName)
-    .then(data => data.json())
+    .then(data => {
+
+      console.log("status ", data.status);
+      console.log("url ", data.url);
+      console.log("statusText ", data.statusText);
+      return data.json();
+
+    })
     .then(function(response) {
       return response.map(function(rep) {
         return rep.name;
       });
-    });
+    })
+    .catch(err => {
+      return err;
+    })
 };
 
 module.exports = getRepos;
--- II.write-tests/09-test-async/async-1.test.js
+++ II.write-tests/09-test-async/async-1.test.js
@@ -1,12 +1,18 @@
 const getRepos = require("./async-1");
+const fetch = require("node-fetch");
+jest.mock("node-fetch");
 
 test("gets a list of repositories names", function() {
   // arrange
+  const func = () => {
+    return [{name: "aws-lambda-starter"}, {name: "dom-ajax-repo"}]
+  }
+  fetch.mockResolvedValue({status: 201, url: "http://www.url.cl", statusText: "created", json: func});
   var url = "https://api.github.com/users/kabaros/repos";
   // act
   return getRepos(url).then(function(result) {
     // assert
-    expect(result).toContain("js-exercises");
+    expect(result).toContain("aws-lambda-starter");
     expect(result).toContain("dom-ajax-repo");
   });
 });
